5f8aba88887be4d0930aed5101555c3d6cb0f5de
Rathi <github email address>
2017-05-09 10:07:01 -0400

    tadpole lab learnt abt delgate in association

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,20 @@
 
 ActiveRecord::Schema.define(version: 20140922151507) do
 
+  create_table "frogs", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
+  end
+
+  create_table "ponds", force: :cascade do |t|
+    t.string "water_type"
+    t.string "name"
+    t.string "color"
+  end
+
+  create_table "tadpoles", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
+  end
+
 end
